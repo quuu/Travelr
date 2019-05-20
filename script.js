a82ebc6d0bfda25a6afaa2b1ed5b57c807b7059e
Andrew Qu <qua@rpi.edu>
2019-05-20 14:45:49 -0400

    added respnosive static side bar to display data about the location

--- script.js
+++ script.js
@@ -36,3 +36,11 @@ function w3_open() {
 function w3_close() {
   document.getElementById("mySidebar").style.display = "none";
 }
+
+$(document).ready(function () {
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+});
