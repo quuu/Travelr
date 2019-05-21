b5e1addb3cbd9a49557b1296c077fe2222a7e4c6
Andrew Qu <qua@rpi.edu>
2019-05-20 20:42:01 -0400

    Sample data, on click event, Vue boiler plate code (#1)
    * created style sheet, now working on sidebar popup
    
    * added respnosive static side bar to display data about the location
    
    * added on event click listener, added sample data

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Travelr
 Light-weight web application to display important locations on a map (travels/vacations) with descriptions and pictures
+
+
+## Requrements:
+
+- A map tile layer service (I use [Mapbox](https://www.mapbox.com/))
+  - `config.js` which contains 
+   ``` 
+        var config = {
+
+          MAPBOX_KEY: 'your key',
+          OPENCAGEDATA_KEY: 'your key'
+  
+        };
+    ```
+  - For simplicity of data entry, I use OpenCageData to fetch the coordinates of a location by name.
+
+
+## Example data.json:
+
+- The format of the JSON data for the application to read in
--- script.js
+++ script.js
@@ -1,18 +1,31 @@
+
+
 var mymap = L.map('mapid').setView([51.505, -0.09], 2);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 10,
-  id: 'mapbox.streets',
-  continuousWorld: false,
-  noWrap: true,
+    id: 'mapbox.streets',
+    continuousWorld: false,
+    noWrap: true,
     accessToken: config.MAPBOX_KEY
 }).addTo(mymap);
 
 var marker = L.marker([51.5, -100]).addTo(mymap);
 
-
 var opencageKey = config.OPENCAGEDATA_KEY;
+
+
+// function that get's triggered when a marker is clicked
+// responsible for updating the vue application with new marker information
+function updateDescription(e) {
+
+
+  console.log("wow you clicked " + e.target.customName);
+
+
+
+}
 
 function createMarkerFromLocation(loc){
 
@@ -21,10 +34,32 @@ function createMarkerFromLocation(loc){
       return response.json();
     })
     .then(function(myJson) {
-      L.marker([myJson.features[0].geometry.coordinates[1], myJson.features[0].geometry.coordinates[0]]).addTo(mymap);
+      let mark = L.marker([myJson.features[0].geometry.coordinates[1], myJson.features[0].geometry.coordinates[0]]).addTo(mymap).on('click', updateDescription);
+      mark.customName = loc;
       return myJson.features[0].geometry.coordinates;
     });
 
 }
 
-createMarkerFromLocation("Toronto").then(function(result){ console.log(result); } );
+//initial marker
+createMarkerFromLocation("Toronto", opencageKey).then(function(result){ console.log(result); } );
+
+
+
+fetch('/sample.json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson){
+    console.log(myJson);
+  });
+
+
+// Vue integration
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: "Hello Vue!"
+  }
+})
+
